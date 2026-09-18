1f36e7367999afa62aa629a79ded57f7b7dbb90e
totoswrap <edoardo.brugnatelli@outlook.com>
2026-09-18 22:02:29 +0200

    Use Crazy Conor Day loading title

--- boot.js
+++ boot.js
@@ -145,8 +145,8 @@
     const perfect = detail.perfect || '+0 points';
     const penalty = normalizePenaltyText(detail.penalty || '0 points');
     crazy.innerHTML = `
-      <div class="boot-crazy-title" aria-label="Crazy Day">
-        <span style="--i:0;--wave-color:#5bc8f5">C</span><span style="--i:1;--wave-color:#6dd87a">r</span><span style="--i:2;--wave-color:#e06c6c">a</span><span style="--i:3;--wave-color:#a374f7">z</span><span style="--i:4;--wave-color:#f07dba">y</span><span class="space" style="--i:5"></span><span style="--i:6;--wave-color:#a374f7">D</span><span style="--i:7;--wave-color:#fb8c5f">a</span><span style="--i:8;--wave-color:#40e4e4">y</span>
+      <div class="boot-crazy-title" aria-label="Crazy Conor Day">
+        <span style="--i:0;--wave-color:#5bc8f5">C</span><span style="--i:1;--wave-color:#6dd87a">r</span><span style="--i:2;--wave-color:#e06c6c">a</span><span style="--i:3;--wave-color:#a374f7">z</span><span style="--i:4;--wave-color:#f07dba">y</span><span class="space" style="--i:5"></span><span style="--i:6;--wave-color:#a374f7">C</span><span style="--i:7;--wave-color:#fb8c5f">o</span><span style="--i:8;--wave-color:#40e4e4">n</span><span style="--i:9;--wave-color:#5bc8f5">o</span><span style="--i:10;--wave-color:#6dd87a">r</span><span class="space" style="--i:11"></span><span style="--i:12;--wave-color:#e06c6c">D</span><span style="--i:13;--wave-color:#a374f7">a</span><span style="--i:14;--wave-color:#f07dba">y</span>
       </div>
       <div class="boot-crazy-logo-wrap" aria-hidden="true">
         <img class="boot-crazy-logo" src="imgs/crazylogo.png" alt="">
